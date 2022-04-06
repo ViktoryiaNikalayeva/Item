3ce905de7473aafc075d999a1e3304a1bf1dd27e
Viktoryia <61502586+ViktoryiaNikalayeva@users.noreply.github.com>
2022-04-06 13:02:42 +0300

    Add files via upload

--- src/test/java/tests/TaskTest.java
+++ src/test/java/tests/TaskTest.java
@@ -15,19 +15,7 @@ import utils.RandomUtils;
 import java.awt.*;
 
 public class TaskTest extends BaseTest {
-
-    @Test
-    public void task1() throws AWTException, InterruptedException {
-        WelcomePage welcomePage = new WelcomePage(By.className("start__link"), "Welcome Page");
-        welcomePage.transitionToRegistrationPage();
-        RegistrationPage registrationPage = new RegistrationPage(By.className("login-form__terms-conditions"), "Registration Page");
-        registrationPage.registrationFormActions(RandomUtils.createEmailPasswordAndDomain(), RandomUtils.createEmailPasswordAndDomain(), RandomUtils.createEmailPasswordAndDomain(), EnumUtils.randomString());
-        ImageInterestsPage imageInterestsPage = new ImageInterestsPage(By.className("avatar-and-interests__upload-button"), "Image and Interests Page");
-        Assert.assertEquals(imageInterestsPage.getUploadImageButtonText(), "upload", "Button text is not equal");
-        imageInterestsPage.uploadImageAndInterestsFormActions();
-        DetailsPage detailsPage = new DetailsPage(By.xpath("//div[@class='page-indicator' and contains(., '3 / 4')]"), "3 / 4");
-        Assert.assertEquals(detailsPage.getThirdPageIndicatorText(), "3 / 4", "No indicator");
-    }
+    
 
     @Test
     public void task2() {
